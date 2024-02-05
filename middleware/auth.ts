51de8c14e6f8ba9f5c51618e90b4d98dad83b6fd
maliksvd <malik.krechiche@gmail.com>
2024-02-04 22:28:07 -0500

    fix:  force the redirection

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
   const user = useSupabaseUser();
-  if (!user.value) {
+  if (!user.value && to.path !== "/auth/login") {
     return navigateTo("/auth/login");
   }
 });
